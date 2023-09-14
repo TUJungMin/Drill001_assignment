66f0e14893a81f26e9dcdf3f3352d28cf418bbbd
TUJungMin <paulkjm92@gmail.com>
2023-09-14 17:21:52 +0900

    Drill1번 과제 제출(2020180002,곽정민)
    Drill1번 과제 제출(2020180002,곽정민)

---
+++ Drill001.py
@@ -0,0 +1,37 @@
+import turtle
+
+
+def turtle_move_w():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_move_a():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_move_s():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_move_d():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape("turtle")
+turtle.stamp()
+
+turtle.onkey(turtle_move_w, 'w')
+turtle.onkey(turtle_move_a, 'a')
+turtle.onkey(turtle_move_s, 's')
+turtle.onkey(turtle_move_d, 'd')
+turtle.onkey(restart, 'Escape')
+
+turtle.listen()
+
